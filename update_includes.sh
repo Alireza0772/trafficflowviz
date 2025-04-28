77530ffbf73e890549c5d27826366b3db694c4a5
Alireza <Alirezasnbr@icloud.com>
2025-04-28 10:00:17 +0330

    Remove obsolete VSCode configuration and script files

--- update_includes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update includes in all source and header files
-find src include -type f \( -name "*.cpp" -o -name "*.hpp" \) | xargs grep -l "trafficflowviz" | while read file; do
-  echo "Updating $file"
-  sed -i '' 's/#include "trafficflowviz\//#include "/g' "$file"
-done 
